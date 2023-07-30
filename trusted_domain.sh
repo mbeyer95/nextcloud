bf4c258ce9c8279b83838cbabfb6834e5839e78b
Prianger <mail@maximilianbeyer.de>
2023-07-30 18:45:16 +0200

    .

--- trusted_domain.sh
+++ trusted_domain.sh
@@ -9,9 +9,6 @@ config_file="/var/www/html/config/config.php"
 sed -i "/0 => '192.168.200.80',/a\    1 => '$new_domain'," $config_file
 
 #
-
-index=1
-
 while true; do
     # Ask the user if they want to add a domain
     read -p "Möchten Sie weitere Domains hinzufügen? (j/n) " answer
@@ -19,7 +16,7 @@ while true; do
         # If they said 'j', ask for the domain
         read -p "Bitte geben Sie Ihre Domain ein: " new_domain
         # Insert the new domain into the trusted_domains array
-        sed -i "/${index} => .*/a\    ${index} => '$new_domain'," $config_file
+        sed -i "/1 => .*/a\    2 => '$new_domain'," $config_file
         # Increase the index for the next domain
         ((index++))
     else
